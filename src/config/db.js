eb72fc82ea12c8a086ac15d4d562f3804c0b4d8a
Bekzodmirzaaliyev <bekzodmirzaaliyev27@gmail.com>
2025-08-05 16:51:22 +0500

    ASD

---
+++ src/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const dbConnection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_CNN, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Database connected");
+    } catch (error) {
+        console.log(error);
+        throw new Error("Database connection failed");
+    }
+};
+
+module.exports = {
+    dbConnection,
+};
---
+++ src/index.js
@@ -0,0 +1,28 @@
+// imports
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { dbConnection } = require("./config/db");
+
+// config
+dotenv.config();
+
+// settings
+const app = express();
+
+// Connection to Database
+dbConnection()
+
+// middlewares
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+
+// routes
+// app.use(require("./routes/index"));
+
+// starting the server
+app.listen(process.env.PORT, () => {
+    console.log(`Server on port ${process.env.PORT}`);
+});
